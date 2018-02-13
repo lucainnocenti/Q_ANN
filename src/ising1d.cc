ca8106900fe1266e704fa4d94e2133554671d86d
Luca Innocenti <lukeinnocenti@gmail.com>
2018-02-13 18:25:59 +0000

    reformat and comments of ising1d.cc

--- src/ising1d.cc
+++ src/ising1d.cc
@@ -23,71 +23,74 @@ All use of these programs is entirely at the user's own risk.
 ################################################################################
 */
 
+#include <complex>
 #include <iostream>
 #include <vector>
-#include <complex>
 
-//Transverse-field Ising model in 1d
-class Ising1d{
+// Transverse-field Ising model in 1d
+class Ising1d {
 
-  //number of spins
+  // number of spins
   const int nspins_;
 
-  //value of the transverse field
+  // value of the transverse field
   const double hfield_;
 
-  //option to use periodic boundary conditions
+  // option to use periodic boundary conditions
   const bool pbc_;
 
-  //pre-computed quantities
-  std::vector<std::complex<double> > mel_;
-  std::vector<std::vector<int> > flipsh_;
+  // pre-computed quantities
+  std::vector<std::complex<double>> mel_;
+  std::vector<std::vector<int>> flipsh_;
 
 public:
-
-  Ising1d(int nspins,double hfield,bool pbc=true):nspins_(nspins),hfield_(hfield),pbc_(pbc){
+  Ising1d(int nspins, double hfield, bool pbc = true)
+      : nspins_(nspins), hfield_(hfield), pbc_(pbc) {
     Init();
   }
 
-  void Init(){
-    mel_.resize(nspins_+1,0.);
-    flipsh_.resize(nspins_+1);
+  void Init() {
+    mel_.resize(nspins_ + 1, 0.);
+    flipsh_.resize(nspins_ + 1);
 
-    for(int i=0;i<nspins_;i++){
-      mel_[i+1]=-hfield_;
-      flipsh_[i+1]=std::vector<int>(1,i);
+    for (int i = 0; i < nspins_; i++) {
+      mel_[i + 1] = -hfield_;
+      flipsh_[i + 1] = std::vector<int>(1, i);
     }
-    std::cout<<"# Using the 1d Transverse-field Ising model with h = "<<hfield_<<std::endl;
+    std::cout << "# Using the 1d Transverse-field Ising model with h = "
+              << hfield_ << std::endl;
   }
 
-  //Finds the non-zero matrix elements of the hamiltonian
-  //on the given state
-  //i.e. all the state' such that <state'|H|state> = mel(state') \neq 0
-  //state' is encoded as the sequence of spin flips to be performed on state
-  void FindConn(const std::vector<int> & state,std::vector<std::vector<int> > & flipsh,std::vector<std::complex<double> > & mel){
+  // Finds the non-zero matrix elements of the hamiltonian
+  // on the given state
+  // i.e. all the state' such that <state'|H|state> = mel(state') \neq 0
+  // state' is encoded as the sequence of spin flips to be performed on state
+  void FindConn(const std::vector<int> &state,
+                std::vector<std::vector<int>> &flipsh,
+                std::vector<std::complex<double>> &mel) {
 
-    mel.resize(nspins_+1);
-    flipsh.resize(nspins_+1);
+    mel.resize(nspins_ + 1);
+    flipsh.resize(nspins_ + 1);
 
-    //assigning pre-computed matrix elements and spin flips
-    mel=mel_;
-    flipsh=flipsh_;
+    // assigning pre-computed matrix elements and spin flips
+    mel = mel_;
+    flipsh = flipsh_;
 
-    //computing interaction part Sz*Sz
-    mel[0]=0.;
+    // computing interaction part Sz*Sz
+    mel[0] = 0.;
 
-    for(int i=0;i<(nspins_-1);i++){
-      mel[0]-=double(state[i]*state[i+1]);
+    // iterate through all the spins, adding the -Z_i Z_j terms
+    for (int i = 0; i < (nspins_ - 1); i++) {
+      mel[0] -= double(state[i] * state[i + 1]);
     }
 
-    if(pbc_){
-      mel[0]-=double(state[nspins_-1]*state[0]);
+    // if `pbc_` was set as true, then the last and first spins are
+    // considered to be connected with each other (in practice, an
+    // additional -Z_1 Z_N energy contribution is added)
+    if (pbc_) {
+      mel[0] -= double(state[nspins_ - 1] * state[0]);
     }
-
   }
 
-  int MinFlips()const{
-    return 1;
-  }
-
+  int MinFlips() const { return 1; }
 };
